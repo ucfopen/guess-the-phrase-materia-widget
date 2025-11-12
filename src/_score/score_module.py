e527a5cb4f4f501682bdb4253b066857884c3a81
Corey Peterson <corey.peterson@ucf.edu>
2025-11-12 14:31:54 -0500

    Tweaked score module. MWDK 3.0.4. Added widget management metadata.

--- src/_score/score_module.py
+++ src/_score/score_module.py
@@ -1,51 +1,34 @@
-import json
-
-from core.models import WidgetQset
 from scoring.module import ScoreModule
+
 
 class Hangman(ScoreModule):
     def __init__(self, play=None):
         super().__init__(play)
-        self.scores = {}
+        options = self.qset.get("options", {})
+        self.wrong_limit = int(options.get("wrong_limit", 6))
+
+        partial_value = str(options.get("partial", False)).lower()
+        self.is_partial = partial_value in ("true", "1", "yes")
 
     def check_answer(self, log):
         question = self.get_question_by_item_id(log.item_id)
-        answer = question["answers"][0]["text"]
-        answer_cleaned = self.clean_str(answer)
-        answer_size = len(answer_cleaned)
+        if not question:
+            return 0
 
-        user_text = log.text if hasattr(log, "text") else log["text"]
-        submitted = self.clean_str(user_text)
-        wrong_limit = self.qset.get("options", {}).get("wrong_limit", 6)
-        is_partial = self.qset.get("options", {}).get("partial", False)
+        answer = self.clean_str(question["answers"][0]["text"])
+        answer_size = len(answer)
 
-        submitted = submitted[:answer_size + wrong_limit - 1]
-        missed = len(set(answer_cleaned) - set(submitted))
+        submitted = self.clean_str(log.text)
+
+        submitted = submitted[:answer_size + self.wrong_limit - 1]
+        missed = len(set(answer) - set(submitted))
 
         if missed:
-            if is_partial:
+            if self.is_partial:
                 return round(100 - ((missed / answer_size) * 100))
             else:
                 return 0
         return 100
 
-    def handle_log_question_answered(self, log):
-        item_id = log.item_id
-        # # skip duplicates logs
-        # if item_id in self.scores:
-        #     return
-
-        score = self.check_answer(log)
-        self.scores[item_id] = score
-        self.total_questions += 1
-        self.verified_score += score
-
     def clean_str(self, s: str):
         return list({c.lower() for c in s if c.isalnum()})
-
-    def get_overview_items(self):
-        return [
-            {"message": "Points Lost", "value": 100 - self.calculated_percent},
-            {"message": "Final Score", "value": self.calculated_percent},
-        ]
-
